77733323e4b362972ef3a6fee4a736fa945f202e
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2023-12-20 00:38:04 +0700

    Implement countdown

--- demo/src/components/SerenBox/index.tsx
+++ demo/src/components/SerenBox/index.tsx
@@ -1,13 +1,39 @@
 import { VStack, Heading, Image, HStack } from "@chakra-ui/react";
 import SectionCard from "../SectionCard";
 import OilCard from "./OilCard";
-import { useContext } from "react";
+import { useCallback, useContext, useEffect, useRef, useState } from "react";
 import { DiffusionOptionContext } from "../../contexts/diffusion-option";
+import Countdown from "react-countdown";
 
 export interface SerenBoxProps {}
 
 const SerenBox = () => {
-    const { slotA, slotB } = useContext(DiffusionOptionContext);
+    const {
+        slotA,
+        slotB,
+        detectionMode,
+        diffusionOption,
+        duration,
+        isSessionRunning,
+        setSessionRunning,
+    } = useContext(DiffusionOptionContext);
+    const ref = useRef<Countdown | null>(null);
+
+    const [date, setDate] = useState(Date.now() + duration * 60 * 1000);
+
+    const stopSession = useCallback(() => {
+        setSessionRunning(false);
+        setDate(Date.now() + duration * 60 * 1000);
+        ref.current?.getApi().stop();
+    }, [duration, setSessionRunning]);
+
+    useEffect(() => {
+        if (!isSessionRunning) {
+            stopSession();
+        } else {
+            ref.current?.getApi().start();
+        }
+    }, [isSessionRunning, stopSession]);
 
     return (
         <SectionCard>
@@ -18,11 +44,38 @@ const SerenBox = () => {
                 >
                     SerenBox
                 </Heading>
+
                 <Image
                     src="/serenbox.png"
                     boxSize="50%"
                     minW="10em"
                 />
+
+                {isSessionRunning && (
+                    <Countdown
+                        ref={ref}
+                        date={date}
+                        onComplete={stopSession}
+                        renderer={({ minutes, seconds }) => {
+                            return (
+                                <Heading
+                                    as="h3"
+                                    size="xl"
+                                    color={
+                                        isSessionRunning
+                                            ? "serena.green.300"
+                                            : "serena.black"
+                                    }
+                                >
+                                    {minutes}:{seconds}
+                                </Heading>
+                            );
+                        }}
+                        onStop={stopSession}
+                        onPause={stopSession}
+                    />
+                )}
+
                 <HStack>
                     <OilCard
                         slot="A"
